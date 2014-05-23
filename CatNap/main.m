53e658f420ae45fc40e8ac9c651e1e5845add48b
Ryoichi Hara <rychhr@gmail.com>
2014-05-23 09:44:12 +0900

    Initial Commit

---
+++ CatNap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CatNap
+//
+//  Created by Ryoichi Hara on 2014/05/23.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
