ca9e40f6f9df39aa10c600937f78848ed08b9302
Ryoichi Hara <rychhr@gmail.com>
2014-06-08 11:30:28 +0900

    Add OldTimeyFilter

---
+++ CatNap/OldTimeyFilter.h
@@ -0,0 +1,13 @@
+//
+//  OldTimeyFilter.h
+//  CatNap
+//
+//  Created by Ryoichi Hara on 2014/06/08.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import CoreImage;
+
+@interface OldTimeyFilter : CIFilter
+
+@end
---
+++ CatNap/OldTimeyFilter.m
@@ -0,0 +1,13 @@
+//
+//  OldTimeyFilter.m
+//  CatNap
+//
+//  Created by Ryoichi Hara on 2014/06/08.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "OldTimeyFilter.h"
+
+@implementation OldTimeyFilter
+
+@end
