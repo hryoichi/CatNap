2d0c27623decf42779970b104a5b66f8ba84020b
Ryoichi Hara <rychhr@gmail.com>
2014-06-09 09:42:19 +0900

    Add CustomTransitionFilter

---
+++ CatNap/CustomTransitionFilter.h
@@ -0,0 +1,13 @@
+//
+//  CustomTransitionFilter.h
+//  CatNap
+//
+//  Created by Ryoichi Hara on 2014/06/09.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import CoreImage;
+
+@interface CustomTransitionFilter : CIFilter
+
+@end
---
+++ CatNap/CustomTransitionFilter.m
@@ -0,0 +1,13 @@
+//
+//  CustomTransitionFilter.m
+//  CatNap
+//
+//  Created by Ryoichi Hara on 2014/06/09.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "CustomTransitionFilter.h"
+
+@implementation CustomTransitionFilter
+
+@end
